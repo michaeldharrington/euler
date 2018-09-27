916089055d381f2107cfe1de6ce60f3bf08750ca
mdh <me@michaeldharrington.com>
2018-09-26 23:38:53 -0400

    5

--- euler5.js
+++ euler5.js
@@ -1,16 +1,44 @@
-
-
-const smallestMultiple = nums => {
-
+function isDivisibleByAll(current, maxNum) {
+    for (let i = 2; i <= maxNum; i++) {
+      if (current % i !== 0) {
+        return false;
+      }
+    }
+    return true;
 }
 
-const makeNums = n => {
+for (let i = 2; i < 1000000000; i = i+2) {
+  if (isDivisibleByAll(i, 20)){
+    console.log(i);
+    break;
+  }
+}
+
+
+
+
+
+
+
+/*
+const smallestMultiple = n => {
   let nums = [];
+
   for (let i = 0; i <= n; i++) {
     if (i % i == 0) {
       nums.push(i);
     }
   }
-}
 
-makeNums(20);
+  for (let n in nums){
+    nums[n] = answer/nums[n];
+    if (nums[n]) {
+      console.log("True");
+    }
+  }
+
+  console.log(nums);
+
+  return nums;
+}
+*/
