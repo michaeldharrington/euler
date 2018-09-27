0d6debb90a47740eca48c8026d11a40e859fc0be
Michael-RobComputer <me@michaeldharrington.com>
2018-09-27 10:37:27 -0400

    5

--- euler5.js
+++ euler5.js
@@ -13,32 +13,3 @@ for (let i = 2; i < 1000000000; i = i+2) {
     break;
   }
 }
-
-
-
-
-
-
-
-/*
-const smallestMultiple = n => {
-  let nums = [];
-
-  for (let i = 0; i <= n; i++) {
-    if (i % i == 0) {
-      nums.push(i);
-    }
-  }
-
-  for (let n in nums){
-    nums[n] = answer/nums[n];
-    if (nums[n]) {
-      console.log("True");
-    }
-  }
-
-  console.log(nums);
-
-  return nums;
-}
-*/
